a0b38257a91f3604054df0c8cb854c430338f947
Doinikov Ilia <ilidoin@gmail.com>
2024-06-29 16:47:39 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('timeButton').addEventListener('click', function() {
+        tg.sendData('get_time');
+    });
+});
