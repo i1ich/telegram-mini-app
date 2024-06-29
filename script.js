7dcc0ab63d74df33245fee9d9186aadc0b22a2e6
Doinikov Ilia <ilidoin@gmail.com>
2024-06-29 19:55:31 +0200

    Added new commands

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let tg = window.Telegram.WebApp;
-
 tg.expand();
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('timeButton').addEventListener('click', function() {
-        tg.sendData('get_time111');
-    });
+    function addButtonListener(id, command) {
+        document.getElementById(id).addEventListener('click', function() {
+            tg.sendData(command);
+        });
+    }
+
+    addButtonListener('startButton', 'start');
+    addButtonListener('mdButton', 'md');
+    addButtonListener('cdButton', 'cd');
+    addButtonListener('lsButton', 'ls');
+    addButtonListener('touchButton', 'touch');
+    addButtonListener('getButton', 'get');
+    addButtonListener('delButton', 'del');
+    addButtonListener('renameButton', 'rename');
+    addButtonListener('timeButton', 'get_time');
 });
