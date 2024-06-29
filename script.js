f659a25ee280fc57fe0936cb363fce0241c7d7b2
Doinikov Ilia <ilidoin@gmail.com>
2024-06-29 21:18:54 +0200

    Added input fields.

--- script.js
+++ script.js
@@ -2,19 +2,31 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 document.addEventListener('DOMContentLoaded', function() {
-    function addButtonListener(id, command) {
+    function addButtonListener(id, command, inputId = null) {
         document.getElementById(id).addEventListener('click', function() {
-            tg.sendData(command);
+            let data = [command];
+            if (inputId) {
+                let inputValue = document.getElementById(inputId).value.trim();
+                if (inputValue) {
+                    if (command === 'rename') {
+                        // Для команды rename разделяем ввод на старое и новое имя
+                        data = data.concat(inputValue.split(',').map(item => item.trim()));
+                    } else {
+                        data.push(inputValue);
+                    }
+                }
+            }
+            tg.sendData(JSON.stringify(data));
         });
     }
 
     addButtonListener('startButton', 'start');
-    addButtonListener('mdButton', 'md');
-    addButtonListener('cdButton', 'cd');
+    addButtonListener('mdButton', 'md', 'mdInput');
+    addButtonListener('cdButton', 'cd', 'cdInput');
     addButtonListener('lsButton', 'ls');
     addButtonListener('touchButton', 'touch');
-    addButtonListener('getButton', 'get');
-    addButtonListener('delButton', 'del');
-    addButtonListener('renameButton', 'rename');
+    addButtonListener('getButton', 'get', 'getInput');
+    addButtonListener('delButton', 'del', 'delInput');
+    addButtonListener('renameButton', 'rename', 'renameInput');
     addButtonListener('timeButton', 'get_time');
 });
